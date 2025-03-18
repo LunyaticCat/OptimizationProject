acae2754f551f6b81daf798d883baa83bdae0e6e
luna <yannbrun1507@outlook.fr>
2025-03-18 11:26:34 +0100

    added solution to ex 5

--- class_example/knack_pack.py
+++ class_example/knack_pack.py
@@ -6,11 +6,11 @@ class Item:
         self.weight = weight
         self.value = value
 
-def solve_knap_sack(n: int, items: list[Item], capacity: int):
+def solve_knap_sack(items: list[Item], capacity: int):
     model = Model("knapsack")
-    take = [model.add_var(var_type=BINARY) for _ in range(n)]
-    model.objective = maximize(xsum(items[i].value * take[i] for i in range(n)))
-    model += xsum(items[i].weight * take[i] for i in range(n)) <= capacity
+    take = [model.add_var(var_type=BINARY) for _ in range(len(items))]
+    model.objective = maximize(xsum(items[i].value * take[i] for i in range(len(items))))
+    model += xsum(items[i].weight * take[i] for i in range(len(items))) <= capacity
 
     status = model.optimize(max_seconds=2)
 
@@ -22,4 +22,6 @@ def solve_knap_sack(n: int, items: list[Item], capacity: int):
                 print(items[i].name, ": Taken")
 
 item_list = [Item("A", 5, 10), Item("B", 7, 13), Item("C", 4, 8), Item("D", 6, 11)]
-solve_knap_sack(4, item_list, 15)
+item_list_2 = [Item("x1", 5, 17), Item("x2", 3, 10), Item("x3", 8, 23), Item("x4", 7, 17)]
+solve_knap_sack(item_list, 15)
+solve_knap_sack(item_list_2, 12) # solution to the exercise 5 problem (branch and bound method)
